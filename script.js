66926b92f746d4cc205dcb49513ead871e818254
sizwe <nkosiczwe@gmail.com>
2022-06-01 21:38:18 +0200

    Add calc 360 degrees rotation (360/12) = 30 deg one hour

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Variables
 let hour = document.getElementById('hour')
 let minute = document.getElementById('minute')
-let second = document.getElementById('seconds')
+let seconds = document.getElementById('seconds')
 
 //Setting the clock
 let set_clock = setInterval(() => {
@@ -10,4 +10,13 @@ let set_clock = setInterval(() => {
     let hr = date_now.getHours();
     let min = date_now.getMinutes();
     let sec = date_now.getSeconds();
-})
+
+    let calc_hr = (hr * 30) + (min / 2);
+    let calc_min = (min * 6) + (sec / 10);
+    let calc_sec = sec * 6;
+
+    hour.style.transform = ` rotate(${calc_hr}deg)`;
+    minute.style.transform = ` rotate(${calc_min}deg)`;
+    seconds.style.transform = ` rotate(${calc_sec}deg)`;
+    
+}, 1000);
