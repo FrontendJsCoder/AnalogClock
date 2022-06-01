1809aa83bbd11168dd9b9dcbc6540c834149885b
sizwe <nkosiczwe@gmail.com>
2022-06-01 21:14:42 +0200

    Declare variables and set clock to run 1000ms for hands to change every second

---
+++ script.js
@@ -0,0 +1,9 @@
+//Variables
+let hour = document.getElementById('hour')
+let minute = document.getElementById('minute')
+let second = document.getElementById('seconds')
+
+//Setting the clock
+let set_clock = setInterval(() => {
+    let date_now = new Date();
+})
