322b97076bc109458009975963d498cfdcba9b9f
sizwe <nkosiczwe@gmail.com>
2022-06-01 21:21:03 +0200

    Create date object, getHours() returns value between 0-24, getMinutes() and getSeconds returns value between 0-59

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ let second = document.getElementById('seconds')
 //Setting the clock
 let set_clock = setInterval(() => {
     let date_now = new Date();
+
+    let hr = date_now.getHours();
+    let min = date_now.getMinutes();
+    let sec = date_now.getSeconds();
 })
